dcfe9a73a0e3952e2c4127844d58c6434bcbda07
Evan <87997759+evanorti@users.noreply.github.com>
2022-11-11 11:05:36 -0800

    Initial commit

---
+++ src/theme/Link/link-data/hyperlinks.ts
@@ -0,0 +1,22 @@
+import { HyperlinkProps } from '../Hyperlink';
+
+const hyperlinks: HyperlinkProps[] = [
+  {
+    href: 'https://station.terra.money/',
+    innerHTML: 'Terra Station',
+  },
+  {
+    href: 'https://status.terra.money/',
+    innerHTML: 'Terra Status',
+  },
+  {
+    href: 'https://finder.terra.money/',
+    innerHTML: 'Terra Finder',
+  },
+  {
+    href: 'https://agora.terra.money/',
+    innerHTML: 'Terra Agora',
+  },
+]
+
+export default hyperlinks;
